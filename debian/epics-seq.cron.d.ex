7cf39bafd2c3181398b1bf4d5c835fa655db53c0
Martin Konrad <konrad@frib.msu.edu>
2016-01-12 11:32:02 -0500

    Customize debian directory

--- debian/epics-seq.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the epics-seq package
-#
-0 4	* * *	root	[ -x /usr/bin/epics-seq_maintenance ] && /usr/bin/epics-seq_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(epics-seq):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="epics-seq" command="/usr/bin/epics-seq"
