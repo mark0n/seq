7c6fff47c7641ebf3b46cde1e4b0747e41bcb68e
Martin Konrad <konrad@frib.msu.edu>
2016-01-12 11:17:46 -0500

    Create debian directory with debhelper
    dh_make --single -p epics-seq_2.2.3

---
+++ debian/epics-seq.default.ex
@@ -0,0 +1,10 @@
+# Defaults for epics-seq initscript
+# sourced by /etc/init.d/epics-seq
+# installed at /etc/default/epics-seq by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(epics-seq):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="epics-seq" command="/usr/bin/epics-seq"
